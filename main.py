53ea48895de051a6792d7e0caa6329ebe7a8e50c
MarcioHideo <marc-ishi@usp.br>
2025-06-01 16:38:48 -0300

    first interface of the calculator

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ class Calculator:
         self.result_content = StringVar(value = "You clicked 0 time.")
         
         # Creating frame
-        frame = ttk.Frame(self.root, padding= 10)
+        # frame = ttk.Frame(self.root, padding= 10)
+        frame = ttk.Frame(self.root, borderwidth=6, relief="ridge", width=200, height=100)
         frame.grid()
 
-        # Creating button
-        button = ttk.Button(frame, text = "Click", command = self.calculate)
-        button.grid(column= 0, row = 0)
+        # Buttons style with ("textValue","row","column")
+        buttons = (
+            ("C", 1, 0), ("±", 1, 1), ("%", 1, 2), ("/", 1, 3),
+            ("7", 2, 0), ("8", 2, 1), ("9", 2, 2), ("×", 2, 3),
+            ("4", 3, 0), ("5", 3, 1), ("6", 3, 2), ("-", 3, 3),
+            ("1", 4, 0), ("2", 4, 1), ("3", 4, 2), ("+", 4, 3),
+            ("0", 5, 0), (".", 5, 1), ("=", 5, 3, 2)
+        )
+
+        # Creating button for calculator
+        for item in buttons:
+            button = ttk.Button(frame, text = item[0], command= self.calculate)
+            button.grid(row=item[1], column = item[2])
+
+
+        # button = ttk.Button(frame, text = "Click", command = self.calculate)
+        # button.grid(column= 0, row = 1)
 
         # Creating Label
         label = ttk.Label(frame, textvariable= self.result_content)
-        label.grid(column = 0, row = 1)
+        label.grid(column = 0, row = 0, columnspan= 5)
 
     # Function to calculate the clicks
     def calculate(self):
