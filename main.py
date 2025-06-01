e3cab684953d08c833c5b1530e0d9245da7e23b5
MarcioHideo <marc-ishi@usp.br>
2025-06-01 17:19:05 -0300

    add all the buttons and operations

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class Calculator:
     def __init__(self, root):
         self.root = root
         self.clicked = 0
-        self.result_content = StringVar(value = "You clicked 0 time.")
+        self.result_content = StringVar(value = "")
+        
         
         # Creating frame
         # frame = ttk.Frame(self.root, padding= 10)
@@ -23,21 +24,40 @@ class Calculator:
 
         # Creating button for calculator
         for item in buttons:
-            button = ttk.Button(frame, text = item[0], command= self.calculate)
+            button = ttk.Button(frame, text = item[0], command=lambda val=item[0]: self.handle_text(val))
             button.grid(row=item[1], column = item[2])
 
 
         # button = ttk.Button(frame, text = "Click", command = self.calculate)
-        # button.grid(column= 0, row = 1)
+        # buttonñ.grid(column= 0, row = 1)
 
         # Creating Label
         label = ttk.Label(frame, textvariable= self.result_content)
         label.grid(column = 0, row = 0, columnspan= 5)
 
-    # Function to calculate the clicks
-    def calculate(self):
-        self.clicked+= 1
-        self.result_content.set(f"You clicked {self.clicked} times.")
+    # Handle all the buttons function
+    def handle_text(self, value):
+        current_value = self.result_content.get()
+        if current_value == "Error":
+            current_value = ""
+
+        if value == "C":
+            self.result_content.set("")
+        elif value == "=":
+            current_value = current_value.replace("×","*")
+            try:
+                result = eval(current_value)
+                self.result_content.set(result)
+            except:
+                self.result_content.set("Error") 
+        elif value == "±":
+            self.resul_content.set(f"{current_value}")
+        elif value == "%":
+            current_value = float(current_value) * 0.01
+            self.result_content.set(f"{current_value}")
+        else:
+            self.result_content.set(f"{current_value}{value}")
+
 
 
 if __name__ == "__main__":
